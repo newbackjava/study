3080b0a49c0f4f34a5c00d91d755ceca4253ae6d
jungwon <hera@naver.com>
2025-04-15 17:15:22 +0900

    자바 기본(참조형)

--- src/main/java/test2/ArrayTest.java
+++ src/main/java/test2/ArrayTest.java
@@ -2,6 +2,77 @@ package test2;
 
 public class ArrayTest {
     public static void main(String[] args) {
+        //같은 타입의 데이터를 많이 사용하고 하는 경우
+        //배열을 가장 많이 사용함.
+        //고정길이 : 처음에 정한 크기 변경 불가
+        //배열 만드는 방법
+        //1. 배열을 만드는 시점에 값목록을 알고 있는 경우
+        int[] y = {1, 2, 3};
+
+        //2. 배열을 만드는 시점에 값목록을 모르다가 나중에 넣는 경우
+        int[] x = new int[3];
+        x[0] = 1;
+        x[1] = 2;
+        x[2] = 3;
+        System.out.println("배열의 원소의 개수 : " + x.length); //3(읽기 전용변수)
+
+        //타입별 배열 만들기
+        //주제 - 가족(나이)
+        int[] ages = {50, 45, 35, 25};
+
+        //c타입의 for문
+        System.out.println("--- c타입의 for출력 ---");
+        for (int i = 0; i < ages.length; i++) {
+            System.out.println(ages[i]);
+        }
+
+        //for-each문(자동으로 반복해서 꺼내옴)
+        System.out.println("--- for-each출력 ---");
+        for(int one : ages){
+            //ages배열에서 하나씩 꺼내 one이라는 변수에 넣어 출력
+            //one이라는 변수에는 ages의 원소 개수만큼 저장되었다가 출력 처리함.
+            System.out.println(one);
+        }
+
+        //주제 - 가족(몸무게)
+        double[] w = {88.1, 52.2, 63.3, 77.2};
+        System.out.println("--- 몸무게 출력 ---");
+        for (int i = 0; i < w.length; i++) {
+            System.out.println(w[i]);
+        }
+        for(double one : w){
+            System.out.println(one);
+        }
+
+        double sum = 0.0;
+        for (double one : w) {
+            sum += one;
+        }
+        System.out.println("---------------");
+        System.out.println("몸무게 평균 " + sum / w.length);
+
+//        배열의 원소의 개수 : 3
+//        --- c타입의 for출력 ---
+//        50
+//        45
+//        35
+//        25
+//        --- for-each출력 ---
+//        50
+//        45
+//        35
+//        25
+//        --- 몸무게 출력 ---
+//        88.1
+//        52.2
+//        63.3
+//        77.2
+//        88.1
+//        52.2
+//        63.3
+//        77.2
+//        ---------------
+//        몸무게 평균 70.2
 
     }
 }
---
+++ src/main/java/test2/ArrayTest2.java
@@ -0,0 +1,31 @@
+package test2;
+
+public class ArrayTest2 {
+    public static void main(String[] args) {
+    //다차원 배열(극장의 각 열마다 자리 번호가 있는 경우)
+    // 1. 배열 만들때 값을 미리 알고 있는 경우
+        int[][] seats = {
+                {1, 2}, //seats[0][0], seats[0][1]
+                {3, 4, 5}, //seats[1][0], seats[1][1], seats[1][2]
+                {6, 7, 8, 9}
+                //seats[2][0], seats[2][1], seats[2][2], seats[2][3]
+        };
+        System.out.println(seats.length); //행의 개수(3)
+        System.out.println(seats[0].length); //1열의 개수(2)
+        System.out.println(seats[0][0]); //1
+
+    // 2. 배열 만들때 값을 아직 모르고 있는 경우
+        int[][] seats2 = new int[3][3];
+        //3행 X 3열인 경우(열의 개수가 동일한 경우)
+        //타입명[행의개수][각 행의 열개수]
+
+        int[][] seats3 = new int[3][];
+        //3행이지만 각 열의 요소의 개수는 미정
+        seats3[0] = new int[] { 1, 2 };
+        seats3[1] = new int[] { 3, 4, 5};
+        seats3[2] = new int[] { 6, 7, 8, 9};
+
+        System.out.println(seats3[2][0]); //6
+
+    }
+}
