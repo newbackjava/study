bbdb25519c5cc9bea63d5cf82fa67bf85b4bdfc8
jungwon <hera@naver.com>
2025-04-21 16:00:40 +0900

    상속

---
+++ src/main/java/object2/CalUse2.java
@@ -0,0 +1,15 @@
+package object2;
+
+import object.Cal2;
+
+public class CalUse2 {
+    public static void main(String[] args) {
+        Cal2 cal2 = new Cal2();
+        //계산기를 만들어서 사용할 수 있음.   
+        //cal2.sum();
+        //default 접근 제어자는 패키지가 달라지면
+        //접근 불가
+
+    }
+}
+
