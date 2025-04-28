f1e1c1cde1fcf1077d4c4bb0bdcca332c8a2d30f
multicampus KDT JavaBackend <134504192+Multicampuswill@users.noreply.github.com>
2025-04-28 14:33:28 +0900

    iterator

--- src/main/java/collection2/IteratorTest.java
+++ src/main/java/collection2/IteratorTest.java
@@ -10,6 +10,8 @@ public class IteratorTest {
         bag.add("A");
         bag.add("B");
         bag.add("C");
+        
+        //set은 index가 없으므로 index를 이용한 접근은 불가능
 //        for (int i = 0; i < bag.size(); i++) {
 //            System.out.println(bag.get(i));
 //        }
@@ -18,6 +20,7 @@ public class IteratorTest {
             System.out.println(s);
         }
 
+        //컬렉션에 반복해서 꺼내주는 iterator(반복자)를 설정한 후 접근 가능
         Iterator<String> it = bag.iterator();
         while (it.hasNext()) {
             String s = it.next();
